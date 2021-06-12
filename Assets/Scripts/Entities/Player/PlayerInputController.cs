c5affaf9ca336788035b23f9f8cc75e71c022845
Philipp Forstner <S1810238022@students.fh-hagenberg.at>
2021-06-12 11:06:46 +0200

    mouswheel player input

--- Assets/Scripts/Entities/Player/PlayerInputController.cs
+++ Assets/Scripts/Entities/Player/PlayerInputController.cs
@@ -19,6 +19,7 @@ namespace Player.PlayerInput
         public InputActionData<float> Attachment1 => attachment1 ?? (attachment1 = new InputActionData<float>(attachment1Action));
         public InputActionData<float> Attachment2 => attachment2 ?? (attachment2 = new InputActionData<float>(attachment2Action));
         public InputActionData<float> Attachment3 => attachment3 ?? (attachment3 = new InputActionData<float>(attachment3Action));
+        public InputActionData<float> Mousewheel => mouswheel ?? (mouswheel = new InputActionData<float>(mousewheelAction));
 
         [SerializeField] private InputActionAsset inputActions;
 
@@ -30,7 +31,8 @@ namespace Player.PlayerInput
         [SerializeField] private InputActionProperty attachment1Action;
         [SerializeField] private InputActionProperty attachment2Action;
         [SerializeField] private InputActionProperty attachment3Action;
-
+        [SerializeField] private InputActionProperty mousewheelAction;
+        
         private InputActionData<float> dash;
         private InputActionData<Vector2> movement;
         private InputActionData<Vector2> mousePointer;
@@ -39,6 +41,7 @@ namespace Player.PlayerInput
         private InputActionData<float> attachment1;
         private InputActionData<float> attachment2;
         private InputActionData<float> attachment3;
+        private InputActionData<float> mouswheel;
 
         private void OnEnable()
         {
---
+++ Assets/Scripts/General/Utilties/UnparentOnStart.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace General.Utilities
+{
+   public class UnparentOnStart : MonoBehaviour
+   {
+      private void Awake()
+      {
+         transform.DetachChildren();
+      }
+   }
+}
